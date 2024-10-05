e631454158e1220c93f2aeea95fddaeff370f637
Magic Man <65839659+Magician357@users.noreply.github.com>
2024-10-05 16:31:33 -0700

    Clean up beambreak slightly

--- src/main/java/frc/robot/subsystems/BeamBreakSensor.java
+++ src/main/java/frc/robot/subsystems/BeamBreakSensor.java
@@ -1,9 +1,6 @@
 package frc.robot.subsystems;
 
-// import frc.robot.Constants.BeamBreakSensorConstants;
-// import frc.robot.Constants.BeamBreakSensorConstants.Ports;
 import edu.wpi.first.wpilibj.DigitalInput;
-// import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
@@ -11,20 +8,35 @@ public class BeamBreakSensor implements Reportable {
     private final int port;
     private final DigitalInput beamBreakSensor;
 
-    private boolean noteDetected;
 
     public BeamBreakSensor(int portIn) {
         port = portIn;
         beamBreakSensor = new DigitalInput(port);
     }
 
-    public boolean noteSensed() {
-        noteDetected = !beamBreakSensor.get();
-        return noteDetected;
-    }
+    // Commented out because this code doesn't do anything usefull and is
+    // inneficient
 
-    public boolean noteSensedWithoutPolling() {
-        return noteDetected;
+    // noteSensed is constatly called by the shuffleboard, so noteDetected
+    // is constantly updated, making noteSensedWithoutPolling useless
+
+    // That means there isn't any use to actually store if a note is 
+    // detected. We should delete these comments and codesoon, I've 
+    // left them in just in case I am wrong.
+
+    // private boolean noteDetected;
+
+    // public boolean noteSensed() {
+    //     noteDetected = !beamBreakSensor.get();
+    //     return noteDetected;
+    // }
+
+    // public boolean noteSensedWithoutPolling() {
+    //     return noteDetected;
+    // }
+
+    public boolean noteSensed(){
+        return !beamBreakSensor.get();
     }
 
     @Override
@@ -33,7 +45,7 @@ public class BeamBreakSensor implements Reportable {
     @Override
     public void initShuffleboard(LOG_LEVEL priority) {
         ShuffleboardTab tab = Shuffleboard.getTab("Indexer");
-        tab.addBoolean("Note Detected", this::noteSensed);
+        tab.addBoolean("Note Detected (port "+port+")", this::noteSensed);
     }
     
 }
