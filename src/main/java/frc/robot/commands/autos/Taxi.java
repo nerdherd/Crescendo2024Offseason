fae1e1d64758ace88686a805e76f3cbbf59a5399
John-Cams <143854397+John-Cams@users.noreply.github.com>
2024-10-12 13:02:53 -0700

    Added taxi auto

---
+++ src/main/java/frc/robot/commands/autos/Taxi.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.autos;
+
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.PathPoint;
+import com.pathplanner.lib.path.RotationTarget;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.SuperStructureConstants;
+import frc.robot.subsystems.SuperSystem;
+import frc.robot.subsystems.swerve.SwerveDrivetrain;
+import frc.robot.subsystems.vision.NoteAssistance;
+
+public class Taxi extends SequentialCommandGroup {
+    
+    // to be tested. Do not use it before test
+
+    public Taxi(SwerveDrivetrain swerve, SuperSystem superSystem, NoteAssistance noteCamera, List<PathPlannerPath> pathGroup){
+        Pose2d startingPose = new Pose2d(1.33, 5.55, new Rotation2d());//GetStartPoseInPath(pathGroup.get(0));
+        addCommands(
+            Commands.runOnce(swerve.getImu()::zeroAll),
+            Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
+            Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
+            AutoBuilder.followPath(pathGroup.get(0))
+        );  
+    }
+}
